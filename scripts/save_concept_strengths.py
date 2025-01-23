a019930bb86ded3fd080bece1418860f3d893ccc
geyan <geyan@ucsd.edu>
2025-01-23 10:24:12 -0800

    Add argument to save concept activations for cc3m

--- scripts/save_concept_strengths.py
+++ scripts/save_concept_strengths.py
@@ -12,9 +12,9 @@ from dncbm.utils import common_init, get_sae_ckpt
 
 def save_concept_strengths(args, is_cc3m=False):
     if is_cc3m:
-        features_path = os.path.join(args.data_dir_activations["img"], args.probe_split)
+        features_path = os.path.join(args.data_dir_activations["img"], f"{args.probe_split}")
     else:
-        features_path = os.path.join(args.probe_data_dir_activations["img"], args.probe_split)
+        features_path = os.path.join(args.probe_data_dir_activations["img"], f"{args.probe_split}.pth")
     all_features = torch.load(features_path)
 
     print(f"Loaded concepts from: {features_path}")
@@ -27,7 +27,7 @@ def save_concept_strengths(args, is_cc3m=False):
     autoencoder = SparseAutoencoder(n_input_features=autoencoder_input_dim, n_learned_features=n_learned_features, n_components=len(args.hook_points)).to(args.device)
 
     autoencoder = get_sae_ckpt(args, autoencoder)
-    all_concepts = None
+    all_concepts = []
 
     with torch.no_grad():
         for features in tqdm(loader):
@@ -35,11 +35,8 @@ def save_concept_strengths(args, is_cc3m=False):
             concepts, reconstructions = autoencoder(features)
             concepts, reconstructions = concepts.squeeze(), reconstructions.squeeze()
            
-            if all_concepts is None:
-                all_concepts = concepts.detach().cpu()
-            else:
-                all_concepts = torch.vstack((all_concepts, concepts.detach().cpu()))
-
+            all_concepts.append(concepts.detach().cpu().to(torch.bfloat16))
+    all_concepts = torch.cat(all_concepts, dim=0)
     whole_all_concepts_fname = os.path.join(args.probe_cs_save_dir, args.probe_split, "all_concepts.pth")
     os.makedirs(osp.dirname(whole_all_concepts_fname), exist_ok=True)
     torch.save(all_concepts, whole_all_concepts_fname)
@@ -47,6 +44,10 @@ def save_concept_strengths(args, is_cc3m=False):
 
 if __name__ == "__main__":
     parser = arg_parser.get_common_parser()
+    parser.add_argument("--cc3m", action="store_true")
     args = parser.parse_args()
     common_init(args)
-    save_concept_strengths(args, is_cc3m= False)
+    if args.cc3m:
+        args.probe_cs_save_dir = args.probe_cs_save_dir.replace(args.probe_dataset, "cc3m")
+    save_concept_strengths(args, is_cc3m=args.cc3m)
+
