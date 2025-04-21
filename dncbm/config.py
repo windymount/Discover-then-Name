69a21c69879f9a17484ea75f5673b80fe972f3ac
geyan <geyan@ucsd.edu>
2025-04-21 14:16:53 -0700

    Update data config

--- dncbm/config.py
+++ dncbm/config.py
@@ -13,11 +13,13 @@ analysis_dir = './analysis'
 
 
 probe_dataset_root_dir_dict = {
-    "places365": "/path/to/dataset/",
-    "imagenet": "/path/to/dataset/",
-    "cifar10": "/path/to/dataset/",
-    "cifar100": "/path/to/dataset/",
+    "places365": "/home/geyan/datasets/places365_torch",
+    "imagenet": "/home/geyan/datasets/imagenet",
+    "cifar10": "/home/geyan/datasets/cifar10",
+    "cifar100": "/home/geyan/datasets/cifar100",
+    "cc3m": "/home/geyan/datasets/cc3m",
+    "cc12m": "/home/geyan/datasets/cc12m",
 }
 
 probe_dataset_nclasses_dict = {"places365": 365,
-                               'imagenet': 1000, "cifar10": 10, "cifar100": 100, }
+                               'imagenet': 1000, "cifar10": 10, "cifar100": 100, "cc3m": 1000, "cc12m": 12000}
